99a57c19b00771118f17eb0fa2ea1fdbcfbe9937
nameof <constexpr@foxmail.com>
2022-08-03 16:40:51 +0800

    JMH评估性能

---
+++ src/test/java/com/nameof/tfidf/TFIDFCalculatorPerformanceTest.java
@@ -0,0 +1,44 @@
+package com.nameof.tfidf;
+
+
+import com.nameof.tfidf.data.DataLoader;
+import com.nameof.tfidf.data.FileDataLoader;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode({Mode.AverageTime, Mode.Throughput})
+@Warmup(iterations = 3, time = 1)
+@Measurement(iterations = 5, time = 5)
+@Threads(4)
+@Fork(1)
+@State(value = Scope.Benchmark)
+@OutputTimeUnit(TimeUnit.SECONDS)
+public class TFIDFCalculatorPerformanceTest {
+
+    DataLoader dataLoader = new FileDataLoader("C:\\Users\\at\\Desktop\\A");
+
+    @Benchmark
+    public void testSerial() {
+        DefaultTFIDFProcessor processor = new DefaultTFIDFProcessor();
+        processor.analyzeAll(dataLoader);
+    }
+
+    @Benchmark
+    public void testParallel() {
+        DefaultTFIDFProcessor processor = new DefaultTFIDFProcessor();
+        processor.setParallel(true);
+        processor.analyzeAll(dataLoader);
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(TFIDFCalculatorPerformanceTest.class.getSimpleName())
+                .build();
+        new Runner(opt).run();
+    }
+}
